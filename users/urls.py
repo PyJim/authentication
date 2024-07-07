663608cf08449fd3ee81cc51d487482918f4b057
Essel Jimmy Kodwo <jimmykodwoessel@gmail.com>
2024-07-07 23:21:17 +0000

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from .views import RegisterView, LoginView, UserDetailView, OrganisationsView, O
 urlpatterns = [
     path('auth/register', RegisterView.as_view(), name='register'),
     path('auth/login', LoginView.as_view(), name='login'),
-    path('api/users/<str:userId>', UserDetailView.as_view(), name='user_detail'),
+    path('api/users/<str:id>', UserDetailView.as_view(), name='user_detail'),
     path('api/organisations', OrganisationsView.as_view(), name='organisations'),
     path('api/organisations/<str:orgId>', OrganisationDetailView.as_view(), name='organisation_detail'),
     path('api/organisations/<str:orgId>/users', AddUserToOrganisationView.as_view(), name='add_user_to_organisation'),
