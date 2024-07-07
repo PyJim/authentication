859a91b557e5ff02e98b132db92f084b1d823860
Jimmy Essel <jimmykodwoessel@gmail.com>
2024-07-07 01:32:22 +0000

    urls changed

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserDetailView, OrganisationListView, OrganisationDetailView, CreateOrganisationView, AddUserToOrganisationView
+from .views import RegisterView, LoginView, UserDetailView, OrganisationsView, OrganisationDetailView, AddUserToOrganisationView
 
 urlpatterns = [
     path('auth/register', RegisterView.as_view(), name='register'),
     path('auth/login', LoginView.as_view(), name='login'),
-    path('api/users/<str:id>', UserDetailView.as_view(), name='user_detail'),
-    path('api/organisations', OrganisationListView.as_view(), name='organisations'),
-    path('api/organisations/<str:org_id>', OrganisationDetailView.as_view(), name='organisation_detail'),
-    path('api/organisations', CreateOrganisationView.as_view(), name='create_organisation'),
-    path('api/organisations/<str:org_id>/users', AddUserToOrganisationView.as_view(), name='add_user_to_organisation'),
+    path('api/users/<str:userId>', UserDetailView.as_view(), name='user_detail'),
+    path('api/organisations', OrganisationsView.as_view(), name='organisations'),
+    path('api/organisations/<str:orgId>', OrganisationDetailView.as_view(), name='organisation_detail'),
+    path('api/organisations/<str:orgId>/users', AddUserToOrganisationView.as_view(), name='add_user_to_organisation'),
 ]
